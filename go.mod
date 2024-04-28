f4852f945ded543c1ba2955c8b54e5130b27517d
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2024-04-28 11:52:30 +0300

    Adding to create User routes

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/kmlcnclk/basic-go-app
 
 go 1.22.2
 
+require github.com/gofiber/fiber/v2 v2.52.4
+
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.52.4 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -6,12 +6,95 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type User struct {
+	Id       string "json:id"
+	Name     string "json:name"
+	Age      int32  "json:age"
+	Email    string "json:email"
+	Password string "json:password"
+}
+
+var users []User
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/hello", func(ctx *fiber.Ctx) error {
+	userGroup := app.Group("/users")
 
-		return ctx.SendStatus(http.StatusOK)
+	userGroup.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Status(http.StatusOK).JSON(users)
+	})
+
+	userGroup.Get("/:userId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+
+		if userId == "" {
+			return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"message": "User ID is required",
+			})
+		}
+
+		for _, user := range users {
+			if user.Id == userId {
+				return ctx.Status(http.StatusOK).JSON(user)
+			}
+		}
+
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "User not found",
+		})
+	})
+
+	userGroup.Post("/", func(ctx *fiber.Ctx) error {
+
+		var user User
+
+		if err := ctx.BodyParser(&user); err != nil {
+			return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"message": "Invalid request",
+			})
+		}
+
+		users = append(users, user)
+
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "User created successfully",
+		})
+	})
+
+	userGroup.Put("/:userId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+
+		for i, user := range users {
+			if user.Id == userId {
+				var u User
+
+				if err := ctx.BodyParser(&u); err != nil {
+					return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+						"message": "Invalid request",
+					})
+				}
+				users[i] = u
+			}
+		}
+
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "User updated successfully",
+		})
+	})
+
+	userGroup.Delete("/:userId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+
+		for i, user := range users {
+			if user.Id == userId {
+				users = append(users[:i], users[i+1:]...)
+			}
+		}
+
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "User deleted successfully",
+		})
 	})
 
 	app.Listen(":3000")
